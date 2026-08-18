74c72792eb4ba4b022882ac6d392d20250da6699
Nandiiiniiii <nandinigoyal1110@gmail.com>
2026-08-18 23:11:29 +0530

    Create ReverseArray.java

---
+++ 02-Basic-Arrays/ReverseArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverse(int[] arr, int n) {
+       for(int i=0;i<n/2;i++)
+       {
+        int tmp=arr[i];
+        arr[i]=arr[n-1-i];
+        arr[n-1-i]=tmp;
+       }
+    }
+}
+
